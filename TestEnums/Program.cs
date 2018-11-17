42cd85da235617253c80ec1c0c3169bf73e859a0
John Lundgren <john.paul.lundgren@gmail.com>
2018-11-17 09:32:18 +0100

    First commit

---
+++ TestEnums/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace TestEnums
+{
+
+    enum ShippingMethod
+    {
+        RegularAirMail = 1,
+        RegisteredAirMail = 2,
+        Express = 3
+    };
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            ShippingMethod method = ShippingMethod.RegisteredAirMail;
+            //Writes RegisteredAirMail. WriteLine calls the ToString() method
+            Console.WriteLine(method);
+
+            //Writes 2
+            Console.WriteLine((int)method);
+
+            //Writes Express
+            Console.WriteLine((ShippingMethod)3);
+
+            int methodId = 1;
+            //Writes RegularAirMail
+            Console.WriteLine((ShippingMethod)methodId);
+
+            string methodName = "RegularAirMail";
+            //Parsing string to a ShippingMethod type
+            var m = (ShippingMethod)Enum.Parse(typeof(ShippingMethod), methodName);
+
+            //Writes RegularAirMail
+            Console.WriteLine(m);
+        }
+    }
+}
